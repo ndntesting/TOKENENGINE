96203322926f5dec8859c887f09cc16a1b3e36be
ndntesting <ndntesting@outlook.com>
2018-06-12 21:03:06 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/TOKENENGINE/*.csproj && dotnet build src/TOKENENGINE/*.csproj
---
+++ src/TOKENENGINE/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TOKENENGINE
+{
+    public class Class1
+    {
+    }
+}
